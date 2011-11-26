8aec085d10ede63393cfec5843b91b6e4c8b6923
Aaron A <a.acerboni@gmail.com>
2011-11-26 14:55:28 +0000

    renamed files to lowercase

---
+++ core/parser.js
@@ -0,0 +1,49 @@
+// Module for accessing parsers. Pass in data and it will understand what format it is.
+// One must supply just what it must be converted to.  
+//  
+// Available parsers :
+// 
+// - XML > JSON
+
+var xml2json    = require('/planet.js/parsers/xml-to-json');
+
+// ## parse
+// > Returns parsed data of a desired format.
+// >  
+// > Parameters :  
+// > 
+// > - `data` : the data which has to be parsed.
+// > - `to` : the desired format.
+// > - `callback`
+
+function parse(data, to, callback) {
+  var dataType = getDatasType(data);
+
+  switch(dataType + ">" + to){
+    case "xml>json" :
+      xml2json.parse(data, function(returnedData){
+        callback(returnedData);
+      });
+      break;
+  }
+}
+
+// Figures out what sort of data format the data is in and therefore what parser
+// to use.
+
+function getDatasType(data) {
+  var type;
+  if(data.search("<?xml version=\"1.0\"") >= 0){
+    type = "xml";
+  }
+
+  try{
+    JSON.parse(data);
+    type = "json";
+  } catch(e){}
+
+  return type;
+}
+
+
+exports.parse = parse;
