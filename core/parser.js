63bac52951dacffdac806479197b347738110617
Aaron A <a.acerboni@gmail.com>
2011-12-06 17:51:17 +0000

    parser now works as an object instance

--- core/parser.js
+++ core/parser.js
@@ -5,45 +5,31 @@
 // 
 // - XML > JSON
 
-var xml2json    = require('/planet.js/parsers/xml-to-json');
+var xml2json = require('/planet.js/parsers/xml-to-json');
 
-// ## parse
-// > Returns parsed data of a desired format.
-// >  
-// > Parameters :  
-// > 
-// > - `data` : the data which has to be parsed.
-// > - `to` : the desired format.
-// > - `callback`
+function Parser() {
 
-function parse(data, to, callback) {
-  var dataType = getDatasType(data);
+  this.parse = function(data, to, callback) {
+    var dataType = getDatasType(data);
+    switch(dataType + ">" + to){
+      case "xml>json" :
+        xml2json.parse(data, callback);
+        break;
+    }
+  };
 
-  switch(dataType + ">" + to){
-    case "xml>json" :
-      xml2json.parse(data, function(returnedData){
-        callback(returnedData);
-      });
-      break;
-  }
+  function getDatasType(data) {
+    var type;
+    if(data.search("<?xml version=\"1.0\"") >= 0){
+      type = "xml";
+    }
+    try{
+      JSON.parse(data);
+      type = "json";
+    } catch (e) {}
+    return type;
+  };
+
 }
 
-// Figures out what sort of data format the data is in and therefore what parser
-// to use.
-
-function getDatasType(data) {
-  var type;
-  if(data.search("<?xml version=\"1.0\"") >= 0){
-    type = "xml";
-  }
-
-  try{
-    JSON.parse(data);
-    type = "json";
-  } catch(e){}
-
-  return type;
-}
-
-
-exports.parse = parse;
+exports.Parser = Parser;
