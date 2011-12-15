af93fb27e7911abe8b56efc3f39ec526711499d4
Aaron A <a.acerboni@gmail.com>
2011-12-15 16:59:10 +0000

    added methods aka request handler modules

---
+++ core/methods.js
@@ -0,0 +1,13 @@
+/*
+  Methods for planet.js mostly used by the router module.
+*/
+
+var mongous = require("mongous").mongous;
+
+// example usage :
+// getEntries("all", "only_pictures", 10, true)
+function getEntries(aggregation, filter_by, amount, newest_first){
+  
+}
+
+exports.methods = null;
