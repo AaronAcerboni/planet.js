c89e0af70308e0b13f21568a0c46d3393bd578eb
Aaron A <a.acerboni@gmail.com>
2011-12-15 16:26:15 +0000

    now utilizes the router module

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 // Server houses the main HTTP listener and also uses the Init module starting up 
 // activities which loop the aggregation process.
 
-var fs         = require('fs'),
-    http       = require('http'),
-    init       = require('/planet.js/core/init');
-
+var init       = require('/planet.js/core/init'),
+    router     = require('/planet.js/core/router'),
+    methods    = require('/planet.js/core/methods'),
+    http       = require('http');
 
 // ### Server initialization
 // Utilizes the init module
+
 init.startActivities("/planet.js/aggregations.json");
 
 // ### HTTP listening loop
+// Utilizes the router module
 
-http.createServer(function(req, res) {
+http.createServer(function(req, res){
   
+  var content = router.route(req, methods);
+  res.writeHead(content.responseCode, content.mimeType);
+  res.end(content.data);
+
 }).listen(8888);
