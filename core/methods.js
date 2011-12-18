7a7f8125b123118e0c0fa102dad378ffeec388e6
Aaron A <a.acerboni@gmail.com>
2011-12-18 12:47:05 +0000

    Barebones GET request which retrieves JSON output

--- core/methods.js
+++ core/methods.js
@@ -1,13 +1,46 @@
-/*
-  Methods for planet.js mostly used by the router module.
-*/
+var mongous = require("mongous").Mongous,
+    parser  = require("/planet.js/core/parser");
 
-var mongous = require("mongous").mongous;
+// Todo :
+// 1. Decide on the url structures order
+// 2. Have parser.js read mime types so I don't need all these conditions
+// 3. Make this method less http opinionated.
 
-// example usage :
-// getEntries("all", "only_pictures", 10, true)
-function getEntries(aggregation, filter_by, amount, newest_first){
+function GET(resource, filter, type, response) {
   
+  var search = {};
+
+  if(resource != null) search.aggregation = resource;
+  if(filter != null) search.aggregation ;
+
+  mongous("test.entries").find(search, function(reply){
+
+    if(reply.documents.length == 0){
+      response.writeHead(404, {"Content-Type" : "text/plain"});
+      response.write("Resource not found");
+      response.end();
+    } else if (type == "application/json"){
+      entries = JSON.stringify(reply.documents);
+      response.writeHead(200, {"Content-type" : type});
+      response.write(entres);
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-type" : "text/html"});
+      response.write("A html representation of the requested data");
+      response.end();
+    }
+
+  });
+
 }
 
-exports.methods = null;
+function POST() {}
+
+function PUT() {}
+
+function DELETE() {}
+
+exports.GET = GET;
+exports.POST = POST;
+exports.PUT = PUT;
+exports.DELETE = DELETE;
