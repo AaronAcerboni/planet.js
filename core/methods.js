cfebd9efca628b8ce4b7f701ca751438a01539bf
aaron <aaron@crunchbang.(none)>
2012-01-04 14:32:15 +0000

    minor, readability

--- core/methods.js
+++ core/methods.js
@@ -1,6 +1,62 @@
 var mongous = require("mongous").Mongous,
     Parser  = require("/planet.js/core/parser").Parser;
 
+// Methods
+
+function GET(aggregation, year, month, type, response) {
+    
+  var parser = new Parser(),
+      search = {};
+
+  // Prepare search criteria
+
+  //  by specific aggregation?
+  if(aggregation != "feeds"){
+    search.aggregation = aggregation;
+  }
+
+  //  by Specific Year or Month?
+  if(year != null && month != null){
+    var year = parseInt(year),
+        month = (parseInt(month)) - 1,
+        end = new Date();
+
+    var start = new Date(year, month, 31);
+
+    search.created_on = {$gte : start, $lt : end };
+  } else if (year != null){
+    var year = parseInt(year),
+        end = new Date();
+
+    var start = new Date(year, 0, 1);
+
+    search.created_on = {$gte : start, $lt : end};
+  }
+
+  // Carry out MongoDB search and reply accordingly
+  // If not unspecified or text/html then it defaults to JSON (for now)
+
+  mongous("test.entries").find(search, function(reply){
+
+    if(reply.documents.length == 0){
+      resourceNotFound(response, aggregation);
+    } else if (type == "text/html" || type == undefined){
+      console.log("Someone has asked for a html representation of " + aggregation);
+      OK(response, "HTML not ready yet", "text/html");
+    } else {
+      parser.parse(reply.documents, "application/json", type, function(data){
+        if(data){
+          OK(response, data, type);
+        } else{
+          unsupportedMediaType(response, type);
+        }
+      });
+    }
+
+  });
+
+}
+
 // Replies
 
 function resourceNotFound(response, resource) {
@@ -25,59 +81,6 @@ function OK(response, content, type){
   response.writeHead( 200, {"Content-type" : type});
   response.write(content);
   response.end();
-}
-
-// Methods
-
-function GET(aggregation, year, month, type, response) {
-    
-  var parser = new Parser(),
-      search = {};
-
-  // Prepare search criteria
-
-  // Specific aggregation
-  if(aggregation != "feeds"){
-    search.aggregation = aggregation;
-  }
-
-  // Specific Year or Month
-  if(year != null && month != null){
-    var year = parseInt(year),
-        month = (parseInt(month)) - 1,
-        end = new Date();
-
-    var start = new Date(year, month, 31);
-
-    search.created_on = {$gte : start, $lt : end };
-  } else if (year != null){
-    var year = parseInt(year),
-        end = new Date();
-
-    var start = new Date(year, 0, 1);
-
-    search.created_on = {$gte : start, $lt : end};
-  }
-
-  mongous("test.entries").find(search, function(reply){
-
-    if(reply.documents.length == 0){
-      resourceNotFound(response, aggregation);
-    } else if (type == "text/html" || type == undefined){
-      console.log("Someone has asked for a html representation of " + aggregation);
-      OK(response, "HTML not ready yet", "text/html");
-    } else {
-      parser.parse(reply.documents, "application/json", type, function(data){
-        if(data){
-          OK(response, data, type);
-        } else{
-          unsupportedMediaType(response, type);
-        }
-      });
-    }
-
-  });
-
 }
 
 exports.GET = GET;
