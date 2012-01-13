c37b002282c8e8f4268c9419c75ccc8b98380e70
Aaron A <a.acerboni@gmail.com>
2012-01-13 23:44:07 +0000

    Revised. Method actions like GET are now independent from the feeds resource handler.

--- core/methods.js
+++ core/methods.js
@@ -2,64 +2,91 @@ var mongous = require("mongous").Mongous,
     Parser  = require("/planet.js/core/parser").Parser,
     build   = require("/planet.js/templates/build");
 
-// Resource methods
+// Methods
 
-function feeds(verb, type, response, aggregation, year, month) {
+var methods = {};
 
-  var parser = new Parser(),
-      search = {};
+methods.get = function(response, type, resource, parameters){
 
-  // Prepare search criteria ...
+  var parser = new Parser();
 
-  if(typeof aggregation == "string" && aggregation != "feeds"){
-    search.aggregation = aggregation;
-    aggregation = "feeds";
+  mongous("test." + resource).find(parameters, function(reply){
+    if(reply.documents.length == 0){
+
+      resourceNotFound(response, resource)
+
+    } else if (type == "text/html" || type == undefined){
+      
+      build.html(resource, reply.documents, function(html){
+        OK(response, html, type);
+      })
+
+    } else {
+      
+      Parser.parse(reply.documents, "application/json", type, function(data){
+        if(data){
+          OK(response, data, type);
+        } else {
+          unsupportedMediaType(response, type);
+        }
+      });
+
+    }
+  });
+
+}
+
+methods.put = function(response, type, resource, parameter){
+  // ...
+}
+
+methods.post = function(response, type, resource, parameter){
+  // ...
+}
+
+methods.delete = function(response, type, resource, parameter){
+  // ...
+}
+
+methods.patch = function(response, type, resource, parameter){
+  // ...
+}
+
+// Resource handlers
+
+function feeds(response, verb, type, tokens) {
+
+  var aggregation = tokens[1],
+      year        = tokens[2],
+      month       = tokens[3],
+      parameters   = {};
+
+  // Package vars for method parameter
+
+  if(aggregation){
+    if(aggregation != "all"){
+      parameters.aggregation = aggregation;      
+    }
   }
 
   // If specific Year or Month
   if(year && month){
 
     var start = new Date(parseInt(year), parseInt(month)-1, 1),
-        end = new Date(parseInt(year), parseInt(month)-1, 31);
+        end   = new Date(parseInt(year), parseInt(month)-1, 31);
 
-    search.date = { $gte : start, $lt : end };
+    parameters.date = { $gte : start, $lt : end };
 
   } else if(year){
 
     var start = new Date(parseInt(year), 0, 1),
-        end = new Date(parseInt(year)+1, 0, 1);
+        end   = new Date(parseInt(year)+1, 0, 1);
 
-    search.date = { $gte : start, $lt : end };
+    parameters.date = { $gte : start, $lt : end };
 
   }
-  console.log(search);
-  // Perform MongoDB search
 
-  mongous("test.entries").find(search, function(reply){
-
-    if(reply.documents.length == 0){
-
-      resourceNotFound(response, aggregation);
-
-    } else if (type == "text/html" || type == undefined){
-      
-      build.html(aggregation, reply.documents, function(html){
-        OK(response, html, "text/html");
-      });
-
-    } else {
-
-      parser.parse(reply.documents, "application/json", type, function(data){
-        if(data){
-          OK(response, data, type);
-        } else{
-          unsupportedMediaType(response, type);
-        }
-
-      });
-    }
-
-  });
+  methods[verb.toLowerCase()](response, type, "feeds", parameters);
 
 }
 
@@ -71,9 +98,9 @@ function OK(response, content, type){
   response.end();
 }
 
-function resourceNotFound(response) {
+function resourceNotFound(response, tokens) {
   response.writeHead( 404, {"Content-type" : "text/plain"});
-  response.write("Resource not found");
+  response.write("Resource not found:" + tokens);
   response.end();
 }
 
