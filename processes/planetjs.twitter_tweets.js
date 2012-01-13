04dde23ef8dba2d33e1f7e5e8b9ef24167282e91
Aaron A <a.acerboni@gmail.com>
2012-01-13 23:42:24 +0000

    minor

--- processes/planetjs.twitter_tweets.js
+++ processes/planetjs.twitter_tweets.js
@@ -3,27 +3,28 @@
   to the storage schema.
 */
 
-var Twitter = require('twitter-node').TwitterNode;
-
-var store,
-    tw;
+var Twitter = require('twitter-node').TwitterNode
+    store   = null,
+    tw      = null;
 
 function main(resources, callback) {
+
   store = callback;
+
   tw = new Twitter({
     user : resources.username,
     password : resources.userpass,
     follow : resources.follow
   });
+
   listen();
 }
 
 function listen(){
-  tw.addListener('Error', function(err){
-    console.log( err.message );
+  tw.addListener('Error', function(e){
+    console.log("process:" + e.message );
   })
   .addListener('tweet', function(tweet){
-    console.log("A new tweet entry has been recieved.");
     mapTweetToCommon(tweet);
   })
   .stream();
@@ -32,28 +33,27 @@ function listen(){
 function mapTweetToCommon(tweet){
   var entry = {};
   
-  // Explore : Twitter's rich meta data in relation to planet.js schema
+  // Explore : Twitter's rich meta data in relation to the planet.js schema
   
   entry.date = new Date(tweet.created_at);
   entry.source_link = "http://twitter.com";
 
   entry.data = {
-    title : null,
-    author_name : "@" + tweet.user.screen_name,
-    author_id : tweet.user.id_str,
+    title : "@" + tweet.user.screen_name + " says",
+    author : tweet.user.id_str,
     link : "https://twitter.com/#!/" + tweet.user.screen_name + "/status/" + tweet.id_str,
     text_summary : tweet.text,
     text_full : tweet.text,
     main_image : tweet.user.profile_image_url_https
   };
-
-  // If tweet.place exists apply it to the schema location field
-
+   
   if(tweet.place){
     entry.data.location = tweet.place.full_name;
   } else {
     entry.data.location = null;
   }
+
+  console.log("process: Grabbed Tweet")
 
   store(entry);
 }
