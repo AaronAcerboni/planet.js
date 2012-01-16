b9d0e57092ef6e911687ae38be894380ca3f2c22
Aaron A <a.acerboni@gmail.com>
2012-01-16 21:15:15 +0000

    increased summary text limit

--- processes/planetjs.rss.js
+++ processes/planetjs.rss.js
@@ -27,18 +27,21 @@ function mapRSStoCommon(json) {
       channelImg = null,
       entries = [];
 
-  if(json.channel.image) channelImg = json.channel.image;
+  if(json.channel.image){
+    if(json.channel.image.url) channelImg = json.channel.image.url;
+  }
 
   for (var i = 0; i < json.channel.item.length; i++) {
 
     entries.push({
       date : new Date(json.channel.item[i].pubDate),
+      source_name : channelTitle,
       source_link : channelLink,
       data : {
         title : json.channel.item[i].title,
         author_name : channelTitle,
         author_id : null,
-        text_summary : json.channel.item[i].description.substring(0,140) + "...",
+        text_summary : json.channel.item[i].description.substring(0,240) + "...",
         text_full : json.channel.item[i].description,
         link : json.channel.item[i].link,
         main_image : channelImg,
