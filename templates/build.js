1a71469f1f31aef990166490456d197f86592f7e
Aaron A <a.acerboni@gmail.com>
2012-01-16 21:13:31 +0000

    improved templating

--- templates/build.js
+++ templates/build.js
@@ -6,8 +6,9 @@ function html(resource, data, callback){
   fs.readFile("/planet.js/templates/" + resource + ".mustache", 'utf-8', function(e, template){
 
     var view = {};
-    view[resource] = data;
-
+    view["resourceName"] = resource;
+    view["resource"] = data;
+    
     callback(mustache.to_html(template, view));
 
   });
