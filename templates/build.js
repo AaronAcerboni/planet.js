694538bb0d650f9aeadcc530844f37657c74d779
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-12 15:55:22 +0000

    correct template referencing

--- templates/build.js
+++ templates/build.js
@@ -1,16 +1,12 @@
 var fs       = require("fs"),
     mustache = require("mustache");
 
-function html(resource, data, callback){  })
-
+function html(resource, data, callback){
   fs.readFile('template/' +resource + '.mustache', 'utf-8', function(e, template){
-    console.log(e);
     var view = {};
     view["resourceName"] = resource;
     view["resource"] = data;
-
     callback(mustache.to_html(template, view));
-
   });
 }
 
