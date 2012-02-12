0fae3c8e18069af6c1de1bb136ccfc53ef4b12fb
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-12 17:00:05 +0000

    removing merge conflict

--- templates/build.js
+++ templates/build.js
@@ -2,17 +2,13 @@ var fs       = require("fs"),
     mustache = require("mustache");
 
 function html(resource, data, callback){
-
-  fs.readFile("templates/" + resource + ".mustache", 'utf-8', function(e, template){
-
+  fs.readFile('templates/' +resource + '.mustache', 'utf-8', function(e, template){
     var view = {};
     view["resourceName"] = resource;
     view["resource"] = data;
-
+    console.log(e);
     callback(mustache.to_html(template, view));
-
   });
-
 }
 
 exports.html = html;
