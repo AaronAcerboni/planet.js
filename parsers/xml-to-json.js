15361a2138bdf7eeec79d88feae64a67a66d43e8
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-16 12:03:45 +0000

    correctly credited xml parser library author

--- parsers/xml-to-json.js
+++ parsers/xml-to-json.js
@@ -1,14 +1,14 @@
 /*
   A parser module which transforms xml to json.
-  Built with buglabs' node-xml2json parser https://github.com/buglabs/node-xml2json
+  Built with Leonidas-from-XIV's node-xml2js https://github.com/Leonidas-from-XIV/node-xml2js/
 */
 
 var xml2js = require('../node_modules/xml2js');
 
 
 function parse(data, callback){
-  (new xml2js.Parser()).parseString( data, function(err, result){
-    if (err) throw err;
+  (new xml2js.Parser()).parseString( data, function(e, result){
+    if (e) throw e;
     callback(result);
   });
 }
