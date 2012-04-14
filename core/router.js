9200472341a36c00a0cd1096b5e3aa50a8cd7012
Aaron Acerboni <a.acerboni@gmail.com>
2012-04-14 20:24:06 +0100

    Blind router to resources.

--- core/router.js
+++ core/router.js
@@ -1,29 +1,38 @@
-var methods = require('./methods');
-
+var methods = require('./methods').methods;
 
 function route(request, response){
   var verb     = request.method,
-      mime     = request.headers["content-type"],
+      mime     = request.headers["content-type"] || 'text/html',
       tokens   = request.url.substr(1).split("/"),
-      resource = tokens[0];
+      resource = tokens[0] || '/';
 
-  if(resource == "public"){
+  tokens.splice(0, 1);
 
-    methods["public"](response, request.url);
-
-  } else if(request.url.match("^/$")){
-
-    methods["feeds"](response, verb, mime, tokens);
-
-  } else if(methods[resource]){
-
-    methods[resource](response, verb, mime, tokens);
-
-  } else {
-
-    methods.resourceNotFound(response, resource);
-
-  }
+  methods[verb]({
+    response: response,
+    resource: ruleCheck(resource),
+    mime: mime,
+    params: tokens
+  });
 }
+
+// Route overrides
+
+var rules = {
+  '/': 'feeds',
+  '': 'feeds',
+  'favicon.ico': 'public'
+};
+
+function ruleCheck(slug) {
+  var targets = Object.keys(rules);
+  for (var i = 0; i < targets.length; i++) {
+    if(targets[i] == slug){
+      return rules[targets[i]];
+    }
+  };
+  return slug;
+}
+
 
 exports.route = route;
