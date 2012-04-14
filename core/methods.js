7d1ce2ede7585fff2a7b77e31f9ef47624d0f29d
Aaron Acerboni <a.acerboni@gmail.com>
2012-04-14 20:24:44 +0100

    Make GET request abstract. Handle Content-types better.

--- core/methods.js
+++ core/methods.js
@@ -1,168 +1,119 @@
-var mongojs = require("mongojs"),
-    Parser  = require("./parser").Parser,
-    build   = require("../templates/build"),
-    fs      = require("fs");
+var mongojs = require('mongojs'),
+    Parser  = require('./parser').Parser,
+    build   = require('../templates/build'),
+    fs      = require('fs');
 
+var methods = {}, resources = {}, RESPONSES;
 
-// Methods
+methods.GET = function(options) {
+    resources[options.resource.toLowerCase()](options.params, options.mime,
+        function (responseCode, mime, content) {
+            options.response.writeHead(responseCode, {'Content-type': mime});
+            if(content) {
+                options.response.end(content);
+            } else {
+                options.response.end('No content returned');
+            }
+        });
+};
 
-var methods = {};
+methods.POST = function(options) {};
 
-methods.get = function(response, type, resource, parameters){
-  var db = mongojs.connect("test", [resource]);
+methods.PUT = function(options) {};
 
-  db[resource].find(parameters).sort({date : -1}, function(e, docs){
-    if(docs.length == 0){
+methods.DELETE = function(options) {};
 
-      resourceNotFound(response, resource)
+methods.POST = function (options) {};
 
-    } else if (type == "text/html" || type == undefined){
+// Hard written resources
+// Ideally they can be dynamically generated from mongo collections
+// Also they don't belong here in methods.
 
-      build.html(resource, docs, function(html){
-        OK(response, html, "text/html");
-      })
+resources.feeds = function (params, mime, cb) {
+    var db = mongojs.connect('test', ['feeds']),
+        query = {},
+        aggregation = params[0],
+        year = params[1],
+        month = params[2];
 
-    } else {
+    if(aggregation && aggregation != 'all'){
+        query.aggregation = aggregation;
+    }
 
-      (new Parser()).parse(docs, "application/json", type, function(data){
-        if(data){
-          OK(response, data, type);
+    if(year && month) {
+        var start = new Date(parseInt(year), parseInt(month)-1, 1),
+            end   = new Date(parseInt(year), parseInt(month)-1, 31);
+        query.date = { $gte : start, $lt : end };
+    } else if (year) {
+        var start = new Date(parseInt(year), 0, 1),
+            end   = new Date(parseInt(year)+1, 0, 1);
+        query.date = { $gte : start, $lt : end };
+    }
+
+    db['feeds'].find(query).sort({date: -1}, respond);
+
+    function respond(err, docs) {
+        if(docs.length == 0){
+            cb(RESPONSES.resourceNotFound);
         } else {
-          unsupportedMediaType(response, type);
+            if(mime == 'text/html'){
+                build.html('feeds', docs, function (html) {
+                    cb(RESPONSES.ok, mime, html);
+                })
+            } else {
+                (new Parser()).parse(docs, "application/json", mime, function(data){
+                    if(data){
+                        cb(RESPONSES.ok, mime, data);
+                    } else {
+                        cb(RESPONSES.resourceNotFound);
+                    }
+                });
+            }
         }
-      });
-
     }
-  });
+};
 
-}
+resources.public = function (params, mime, cb) {
+    var path = params[0],
+        file = params[1],
+        type, readAs;
 
-methods.put = function(response, type, resource, parameter){
-  // ...
-}
-
-methods.post = function(response, type, resource, parameter){
-  // ...
-}
-
-methods.delete = function(response, type, resource, parameter){
-  // ...
-}
-
-methods.patch = function(response, type, resource, parameter){
-  // ...
-}
-
-// Resource handlers
-
-function feeds(response, verb, type, tokens) {
-
-  var aggregation = tokens[1],
-      year        = tokens[2],
-      month       = tokens[3],
-      parameters   = {};
-
-  // Package vars for method parameter
-
-  if(aggregation){
-    if(aggregation != "all"){
-      parameters.aggregation = aggregation;
+    switch(path){
+        case 'images':
+            type = 'binary';
+        break;
+        case 'css':
+            readAs = 'utf-8';
+            type = 'text/css';
+        break;
+        case 'js':
+            readAs = 'utf-8';
+            type = 'text/javascript';
+        break;
     }
-  }
-
-  // If specific Year or Month
-  if(year && month){
-
-    var start = new Date(parseInt(year), parseInt(month)-1, 1),
-        end   = new Date(parseInt(year), parseInt(month)-1, 31);
-
-    parameters.date = { $gte : start, $lt : end };
-
-  } else if(year){
-
-    var start = new Date(parseInt(year), 0, 1),
-        end   = new Date(parseInt(year)+1, 0, 1);
-
-    parameters.date = { $gte : start, $lt : end };
-
-  }
-
-  methods[verb.toLowerCase()](response, type, "feeds", parameters);
-
+    type = type || mime;
+    fs.readFile('public/' + path + '/' + file, readAs, function (err, data) {
+        if(err){
+            if(err.errno === 34){
+                cb(RESPONSES.resourceNotFound);
+            } else {
+                cb(RESPONSES.error);
+            }
+        } else {
+            cb(RESPONSES.ok, type, data);
+        }
+    });
 }
 
-// Public assets handler
+// Responses
 
-function public(response, path) {
-  var read = null,
-      type = null,
-      dir  = path.substr(1).split('/')[1];
+RESPONSES = {
+    ok: 200,
+    resourceNotFound: 404,
+    unsupportedMediaType: 415,
+    unsupportedMethod: 405,
+    forbidden: 403,
+    error: 500
+};
 
-  if(dir == 'images'){
-    type = 'binary';
-  } else {
-    read = 'utf-8';
-    switch(dir){
-      case 'css': type = 'text/css'; break;
-      case 'js' : type = 'text/javascript'; break;
-    }
-  }
-
-  fs.readFile(path.substr(1), read, function(e, data){
-    if(e){
-      if(e.errno == 34){
-        resourceNotFound(response, path);
-      } else {
-        error(response, path);
-      }
-    } else {
-      OK(response, data, type);
-    }
-  });
-
-}
-
-// Replies
-
-function OK(response, content, type){
-  response.writeHead( 200, {"Content-type" : type});
-  response.write(content);
-  response.end();
-}
-
-function resourceNotFound(response, path) {
-  response.writeHead( 404, {"Content-type" : "text/plain"});
-  response.write("Resource not found:" + path);
-  response.end();
-}
-
-function unsupportedMediaType(response, type) {
-  response.writeHead( 415, {"Content-type" : "text/plain"});
-  response.write("Unsupported media type: " + type);
-  response.end();
-}
-
-function unsupportedVerb(response, verb) {
-  response.writeHead( 405, {"Content-type" : "text/plain"});
-  response.write("Unsupported http verb or no verb supplied: " + verb);
-  response.end();
-}
-
-function forbidden(response, path) {
-  response.writeHead( 403, {"Content-type" : "text/plain"});
-  response.write("Forbidden: " + path);
-  response.end();
-}
-
-function error(response, path) {
-  response.writeHead( 500, {"Content-type" : "text/plain"});
-  response.write("The server encounter a problem relating to the path: " + path);
-  response.end();
-}
-
-// Interface
-
-exports.resourceNotFound = resourceNotFound;
-
-exports.feeds = feeds;
-exports.public = public;
+exports.methods = methods;
